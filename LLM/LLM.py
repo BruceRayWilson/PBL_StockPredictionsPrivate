354ea6cd85694cbfb5b6576f2cccf643c309f26c
Bruce Ray Wilson <BruceRayWilson42@gmail.com>
2023-11-03 18:52:57 -0600

    A-I with three rolling averages.  26 letters?

--- LLM/LLM.py
+++ LLM/LLM.py
@@ -86,7 +86,7 @@ class LLM:
             learning_rate=2e-4,
             per_device_train_batch_size=512,
             per_device_eval_batch_size=512,
-            num_train_epochs=1,
+            num_train_epochs=5,
             weight_decay=0.01,
             evaluation_strategy = "epoch",
             logging_strategy="epoch"
